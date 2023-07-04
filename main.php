5b83816a59800934becbe708137fb98259795da8
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-07-04 22:17:43 +0900

    add : aditional task

--- main.php
+++ main.php
@@ -1,8 +1,19 @@
 <?php
 
-$word = fgets(STDIN);
+// $word = fgets(STDIN);
+$word = $argv[1];
+$count = 0;
+if ($word < "A" || $word > "Z") {
+    $msg_error = 'standard error' . PHP_EOL;
+    fputs(STDERR, $msg_error);
+    exit();
+}
+
 for ($i = ord($word); $i <= ord("Z"); $i++) {
+    $count++;
     echo chr($i);
 }
 
+echo PHP_EOL;
+echo $count;
 ?>
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-alpha = input() 
+import sys
+
+# alpha = input() 
+alpha = sys.argv[1]
+cnt = 0
+if alpha < 'A' or alpha > 'Z':
+  print("standard error", file=sys.stderr)
+  exit(1)
+
+
 for i in range(ord(alpha),ord('Z')+1):
+  cnt+=1
   print(chr(i),end="")
+
+print()
+print(cnt)
